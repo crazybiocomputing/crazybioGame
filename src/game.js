764ac0d12c9052a5b4a13e720d35602b73151da9
jeesay <jjeesay@gmail.com>
2015-05-12 16:13:56 +0200

    Update game.js

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@
  *  crazybioGame: CrazyBioComputing Game Engine
  *  Copyright (C) 2015  Jean-Christophe Taveau.
  *
- *  This file is part of crazybioGE
+ *  This file is part of crazybioGame
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
