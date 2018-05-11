9521374e3b17289c0be57a475a61b878b3230ebe
CharlotteGF <34349999+CharlotteGF@users.noreply.github.com>
2018-05-11 22:44:58 +0200

    Update lock.js

--- src/core/lock.js
+++ src/core/lock.js
@@ -28,26 +28,45 @@
 
 /**
  * Lock displayed as a Text Field
+ * @author Hans SCHRIEKE
  */
 const createLockText = (props) => {
   let element = document.createElement('div');
   element.id = props.id;
   element.className = "lockText";
-  
-  // TODO
-  
+
+  createPopUp(props,"lockText");
+  let modal = document.getElementById('lockText');
+  let button = document.getElementById(`svg_${props.id}`);
+  button.type = "submit";
+  let closeB = document.getElementsByClassName('close'+"lockText")[0];
+
+  button.onclick = function() {
+    modal.style.display = "block";
+
+  }
+  closeB.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+  }
+
   return element;
 };
 
 /**
  * Lock displayed as a Numerical Field
-  * @author SCHRIEKE Hans
  */
+
+
 const createLockNumerical = (props) => {
   let element = document.createElement('div');
   element.id = props.id;
   element.className = "lockNumerical";
-  
+
   createPopUp(props,"lockNum");
   let modal = document.getElementById('lockNum');
   let button = document.getElementById(`svg_${props.id}`);
@@ -72,6 +91,7 @@ const createLockNumerical = (props) => {
   return element;
 };
 
+
 /**
  * Lock displayed as a n-Digit Lock
  */
@@ -79,107 +99,21 @@ const createLockDigit = (nDigits) => (props) => {
   let element = document.createElement('div');
   element.id = props.id;
   element.className = "lockDigit";
-  
+
   // TODO
-  
+
   return element;
 };
 
 /**
  * Lock displayed as a keypad
- * @author TODO
  */
 const createLockKeypad = (props) => {
   let element = document.createElement('div');
   element.id = props.id;
   element.className = "lockKeypad";
-};
 
-/**
- * Lock displayed as a keypad
- * @author P. Wintringer
- */
-const createLockNumpad = (props) => {
-  let form = document.createElement('form');
-  let element = document.createElement('div');
-  element.id = props.id;
-  element.className = "lockNumpad";
-  //let evt = document.createEvent("MouseEvents");
-  //evt.initMouseEvent("click");
-  let text = document.getElementById("textfield");
-  let table = document.createElement('table');
-  let button1 = document.createElement('input')
-  button1.setAttribute('type','button');
-  button1.setAttribute('value',1);
-  button1.onClick = AddValueToTextField(value);
-  table.appendChild(button1);
-  let button2 = document.createElement('input');
-  button2.setAttribute('type','button');
-  button2.setAttribute('value',2);
-  button2.onClick = AddValueToTextField(value);
-  table.appendChild(button2);
-  let button3 = document.createElement('input');
-  button3.setAttribute('type','button');
-  button3.setAttribute('value',3);
-  button3.onClick = AddValueToTextField(value);
-  table.appendChild(button3);
-  let button4 = document.createElement('input');
-  button4.setAttribute('type','button');
-  button4.setAttribute('value',4);
-  button4.onClick = AddValueToTextField(value);
-  table.appendChild(button4);
-  let button5 = document.createElement('input');
-  button5.setAttribute('type','button');
-  button5.setAttribute('value',5);
-  button5.onClick = AddValueToTextField(value);
-  table.appendChild(button5);
-  let button6 = document.createElement('input');
-  button6.setAttribute('type','button');
-  button6.setAttribute('value',6);
-  button6.onClick = AddValueToTextField(value);
-  table.appendChild(button6);
-  let button7 = document.createElement('input');
-  button7.setAttribute('type','button');
-  button7.setAttribute('value',7);
-  button7.onClick = AddValueToTextField(value);
-  table.appendChild(button7);
-  let button8 = document.createElement('input');
-  button8.setAttribute('type','button');
-  button8.setAttribute('value',8);
-  button8.onClick = AddValueToTextField(value);
-  table.appendChild(button8);
-  let button9 = document.createElement('input');
-  button9.setAttribute('type','button');
-  button9.setAttribute('value',9);
-  button9.onClick = AddValueToTextField(value);
-  table.appendChild(button9);
-  let buttonC = document.createElement('input');
-  buttonC.setAttribute('type','button');
-  buttonC.setAttribute('value','C');
-  buttonC.onClick = SupprValueInTextField();
-  table.appendChild(buttonC);
-  let button0 = document.createElement('input');
-  button0.setAttribute('type','button');
-  button0.setAttribute('value', 0);
-  button0.onClick = AddValueToTextField(value);
-  table.appendChild(button0);
-  let buttonV = document.createElement('input');
-  buttonV.setAttribute('type','button');
-  buttonV.setAttribute('value','V');
-  buttonV.onClick = Submit();
-  table.appendChild(buttonV);
-  
-  function AddValueToTextField(val)
-    {document.getElementByID(text).value += val;}
+  // TODO
 
-  function SupprValueInTextField()
-    {document.getElementByID(text).value -= 1;}
-
-  function Submit()
-    {;}
-  
   return element;
 };
-
-
-
