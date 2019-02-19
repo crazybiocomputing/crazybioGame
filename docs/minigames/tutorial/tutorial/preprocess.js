3bea730a83da3dec04656061ceeb56f0d19fb220
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2019-02-19 14:47:32 +0100

    Update tutorial

--- docs/minigames/tutorial/tutorial/preprocess.js
+++ docs/minigames/tutorial/tutorial/preprocess.js
@@ -5,7 +5,7 @@ let code = Math.floor(1000.0 + Math.random()* 8999.0);
 let codeString = code.toString().split('').reduce( (accu,char) => accu + ' ' + char,'');
 console.log(code,codeString);
 
-// Create the corresponding sprite
+// Create the sprite generating the secret code...
 let unlock = createSprite({
   id: 33,
   class: "sprite", 
@@ -28,30 +28,8 @@ let unlock = createSprite({
   }
 });
 
-let lock = createLockNumerical({
-  id : 44,
-  class: "machine.lockNumerical", 
-  description: "A lock symbol", 
-  display: {
-    position: [370,100],
-    width: 70,
-    height: 70,
-    graphics: {
-      path: "../../assets/lock_symbol.png",
-      style: {
-        "display": "none"
-      }
-    },
-    target: {
-      data : ["C",35,35,35]
-    }
-  },
-  features: {
-    exit: code
-  }
-});
 
 // Add it in CRAZYBIOGAME
 CRAZYBIOGAME.deferred[unlock.id] = unlock;
-CRAZYBIOGAME.deferred[lock.id] = lock;
+CRAZYBIOGAME.deferred['code'] = code;
 
