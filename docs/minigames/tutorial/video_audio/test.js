6694af06d58f913c91452b97e76d49e2a3597e5a
MarinaBoudin <marina.boudin@etu.u-bordeaux.fr>
2019-04-05 16:17:13 +0200

    Test

--- docs/minigames/tutorial/video_audio/test.js
+++ docs/minigames/tutorial/video_audio/test.js
@@ -9,7 +9,7 @@ Descritpion partie 2 : Fonction test pour le preprocess , pour l'import au débu
 
 const displayMedias = (medias) => {
   let display = "block";
-  console.log("image1");
+  console.log(medias["img"]);
   for (let i=0;i<medias["img"].length;i++){
     let src = medias["img"][i];
     console.log("image");
