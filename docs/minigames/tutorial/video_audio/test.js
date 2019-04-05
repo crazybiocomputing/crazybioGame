1c4cec8dd3299512a7a33c454b7c417fc82ff2a2
MarinaBoudin <marina.boudin@etu.u-bordeaux.fr>
2019-04-05 14:20:58 +0200

    test load medias

--- docs/minigames/tutorial/video_audio/test.js
+++ docs/minigames/tutorial/video_audio/test.js
@@ -46,17 +46,12 @@ const process = (storyboard) => {
 }
 
 //Partie 2
-onload=function(){
-  var storyboard=this.response;
-  console.log(storyboard);
-  return storyboard;
-}
+
 var request = new XMLHttpRequest();
 request.open('GET',"storyboard.json");
 request.responseTYpe='json';
 request.send();
-let storyboard;
-storyboard =request.onload;
+var storyboard=request.response;
 console.log(storyboard);
 
 /*var storyboard =[{"id": 1,"class": "scene","description": "A poster...","display": {"width": 690,"height": 480,"audio": {"path": "Audio.mp3"}},"children":[2,4,20,21]},{"id": 2,"class": "machine.lockText","description": "A lock symbol","display": {"position": [530,90],"width": 60,"height": 60,"video": {"path": "Video.mp4"},"target" : {"data": ["C", 30,30, 30]}},"features": {"exit": "allright"}},{"id": 20,"class": "machine.tile","description": "Some text","display": {"width": 335,"height": 195,"position": [1, 0],"graphics": {"path": "assets/congratulations_0.jpg"}}}];
