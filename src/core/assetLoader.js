cd29c019cbbb0dce00286882ca04ef7d5c17435e
Domitille COQ--ETCHEGARAY <ayeshafarron@gmail.com>
2019-05-09 10:56:27 +0200

    Fix obj.display

--- src/core/assetLoader.js
+++ src/core/assetLoader.js
@@ -44,32 +44,34 @@ class AssetLoader {
 
     // Function to get media types (image,audio, video, and svg)
     const getTypes = (keys) => {
-        const types = {
-          image: "img",
-          audio: "audio",
-          svg: "svg",
-          video: "video"
-        }
-      let filtered = keys.filter(( keyword) => Object.keys(types).includes(keyword));
+      const types = {
+        image: "img",
+        audio: "audio",
+        svg: "svg",
+        video: "video"
+      }
+      let filtered = keys.filter((keyword) => Object.keys(types).includes(keyword));
       return types[filtered[0]];
     }
 
     // Function to get one asset
-    const getAsset = (accu,obj) => {
-      if (obj.display.media !== undefined) {
-        let asset = {
-          id: obj.id,
-          path: obj.display.media.image ||  obj.display.media.svg || obj.display.media.video || obj.display.media.audio || "none",
-          type: getTypes(Object.keys(obj.display.media)) || "none"
+    const getAsset = (accu, obj) => {
+      if (obj.class !== "deferred" && obj.author == undefined){
+        if (obj.display.media !== undefined) {
+          let asset = {
+            id: obj.id,
+            path: obj.display.media.image || obj.display.media.svg || obj.display.media.video || obj.display.media.audio || "none",
+            type: getTypes(Object.keys(obj.display.media)) || "none"
+          }
+          accu.push(asset);
         }
-        accu.push(asset);
       }
       return accu;
     }
 
     //////////////: MAIN ://////////////
 
-    let assets = json.reduce(getAsset,[]);
+    let assets = json.reduce(getAsset, []);
     console.log(assets);
     return assets;
   }
@@ -78,10 +80,10 @@ class AssetLoader {
 
   /**
    * Pre-load all media
-   */  
+   */
   preload() {
     let all_promises = [];
-    this.assets.forEach( (asset,index) => {
+    this.assets.forEach((asset, index) => {
       all_promises.push(this.fetch_media(asset));
     });
     console.log(all_promises);
@@ -93,34 +95,34 @@ class AssetLoader {
    * Pre-load single media
    */
   fetch_media(media) {
-    if (media.type==="svg") {
-      return fetch(media.path).then( (response) => {
+    if (media.type === "svg") {
+      return fetch(media.path).then((response) => {
         return response.text();
       })
-      .then( (svg) => {
-        document.getElementById("media").insertAdjacentHTML("afterbegin",svg);
-        console.log('SVG');
-        console.log(document.getElementById("media").firstChild);
-        document.getElementById("media").firstChild.nextElementSibling.id = `asset_${media.id}`;
-        return 'loaded';
-      });
+        .then((svg) => {
+          document.getElementById("media").insertAdjacentHTML("afterbegin", svg);
+          console.log('SVG');
+          console.log(document.getElementById("media").firstChild);
+          document.getElementById("media").firstChild.nextElementSibling.id = `asset_${media.id}`;
+          return 'loaded';
+        });
     }
     else {
       return fetch(media.path)
-      .then( (response) => {
-        return response.blob();
-      })
-      .then ( (myBlob) => {
-        let objectURL = URL.createObjectURL(myBlob);
-        let media_html=document.createElement(media.type);
-        media_html.src=objectURL;
-        media_html.id=`asset_${media.id}`;
-        media_html.dataset.src=media.path;
-        let div_media = document.getElementById("media");
-        div_media.appendChild(media_html);
-        console.log(`Create ${media_html.id}`);
-        return 'loaded';
-      });
+        .then((response) => {
+          return response.blob();
+        })
+        .then((myBlob) => {
+          let objectURL = URL.createObjectURL(myBlob);
+          let media_html = document.createElement(media.type);
+          media_html.src = objectURL;
+          media_html.id = `asset_${media.id}`;
+          media_html.dataset.src = media.path;
+          let div_media = document.getElementById("media");
+          div_media.appendChild(media_html);
+          console.log(`Create ${media_html.id}`);
+          return 'loaded';
+        });
     }
   }
 
